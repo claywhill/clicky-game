1e1fa4a86db5ccf280cb12b9eec109088af0d622
Clayton Hill <clayman0123@yahoo.com>
2020-06-12 16:52:08 -0600

    added components with styling

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import SubHeader from "./components/Sub-header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div>
       <Header />
+      <SubHeader />
+      <Main />
     </div>
   )
 }
--- clicky-game/src/components/Header.js
+++ clicky-game/src/components/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Header() {
-  return <header>I'm a header</header>
+  return <header>
+    <h1>Clicky Game</h1>
+    <p>Click an image to begin!</p>
+    <ul>
+      <li className="score">Score: 0</li>
+      <li className="top-score">Top Score: 0</li>
+    </ul>
+  </header>
 }
 
 export default Header;
